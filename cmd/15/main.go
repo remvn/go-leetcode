df0b54ecc31b675bbd5067e2ff093afc56c430a3
remvn <34063162+remvn@users.noreply.github.com>
2024-02-26 01:56:26 +0700

    problem 15

--- cmd/15/main.go
+++ cmd/15/main.go
@@ -1,8 +1,63 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func main() {
+	tests := [][]int{
+		{-1, 0, 1, 2, -1, -4},
+		{0, 1, 1},
+		{0, 0, 0},
+		{-2, 0, 0, 2, 2},
+	}
+	for _, test := range tests {
+		fmt.Println(test)
+		fmt.Println(threeSum(test))
+	}
 }
 
 func threeSum(nums []int) [][]int {
-	return nil
+	var result [][]int
+
+	if len(nums) < 3 {
+		return result
+	}
+
+	sort.Ints(nums)
+
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i-1] == nums[i] {
+			continue
+		}
+
+		left := i + 1
+		right := len(nums) - 1
+		for left < right {
+			target := nums[left] + nums[right] + nums[i]
+			if target == 0 {
+				result = append(result, []int{nums[left], nums[right], nums[i]})
+				left++
+				right--
+
+				// check if next element is the same
+				// the skip it
+				for left < right && nums[left-1] == nums[left] {
+					left++
+				}
+				for right > left && nums[right+1] == nums[right] {
+					right--
+				}
+			} else {
+				if target > 0 {
+					right--
+				} else {
+					left++
+				}
+			}
+		}
+	}
+
+	return result
 }
