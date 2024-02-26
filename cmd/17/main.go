1e3c0839e7aec918c0b0d1f2520969268ba4b4a0
remvn <34063162+remvn@users.noreply.github.com>
2024-02-26 08:26:17 +0700

    problem 17

---
+++ cmd/17/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	digits := []string{"23", "", "2"}
+	for _, digit := range digits {
+		fmt.Println(digit)
+		fmt.Println(letterCombinations(digit))
+		fmt.Println()
+	}
+}
+
+func letterCombinations(digits string) []string {
+	result := []string{}
+	if digits == "" {
+		return result
+	}
+
+	keyboard := map[rune]string{
+		'2': "abc",
+		'3': "def",
+		'4': "ghi",
+		'5': "jkl",
+		'6': "mno",
+		'7': "pqrs",
+		'8': "tuv",
+		'9': "wxyz",
+	}
+
+	var backtracking func(digits string, subset []rune)
+	backtracking = func(digits string, subset []rune) {
+		if digits == "" {
+			result = append(result, string(subset))
+		} else {
+			letters := keyboard[rune(digits[0])]
+			for _, letter := range letters {
+				// we could the code in the comment to
+				// have a better understanding
+				// subset = append(subset, letter)
+				// backtracking(digits[1:], subset)
+				// subset = slices.Delete(subset, len(subset)-1, len(subset))
+				backtracking(digits[1:], append(subset, letter))
+			}
+		}
+	}
+
+	backtracking(digits, nil)
+
+	return result
+}
