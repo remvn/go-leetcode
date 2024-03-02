e17d226aae31c97f575d000c0a2a61dd3a5c7264
remvn <34063162+remvn@users.noreply.github.com>
2024-03-02 12:35:20 +0700

    refactor

--- cmd/33/main.go
+++ cmd/33/main.go
@@ -9,17 +9,22 @@ func main() {
 			target: 0,
 		},
 		{
-			arr:    []int{4, 5, 6, 7, 0, 1, 2},
-			target: 3,
+			arr:    []int{4, 5, 6, 7, 8, 1, 2, 3},
+			target: 8,
 		},
 		{
 			arr:    []int{3, 1},
 			target: 1,
 		},
+		{
+			arr:    []int{4, 1, 2, 3},
+			target: 1,
+		},
 	}
 	for _, test := range tests {
-		fmt.Println(test)
-		fmt.Println(search(test.arr, test.target))
+		fmt.Printf("%+v: \n", test)
+		fmt.Printf("result: %d\n", search(test.arr, test.target))
+		fmt.Println()
 	}
 }
 
@@ -34,6 +39,7 @@ func search(nums []int, target int) int {
 	pivot := searchForPivotPoint(nums)
 
 	if pivot >= 0 {
+		fmt.Printf("Pivot: %d \n", pivot)
 		// check if target is inside pivot point
 		if target >= nums[0] && target <= nums[pivot] {
 			right = pivot
@@ -46,8 +52,8 @@ func search(nums []int, target int) int {
 		mid := left + (right-left)/2
 		if target > nums[mid] {
 			left = mid + 1
-		} else if target < nums[mid] {
-			right = mid - 1
+		} else if target <= nums[mid] {
+			right = right - 1
 		} else if target == nums[mid] {
 			left = mid
 			break
@@ -61,6 +67,8 @@ func search(nums []int, target int) int {
 	}
 }
 
+// idea: search for index of smallest number
+// and substract 1
 func searchForPivotPoint(nums []int) int {
 	// this array isn't rotated
 	if nums[0] < nums[len(nums)-1] {
@@ -69,14 +77,32 @@ func searchForPivotPoint(nums []int) int {
 
 	left := 0
 	right := len(nums) - 1
-	// 4,5,6,7,0,1,2
+
+	//  4 5 6 7 8 1 2 3
+	//  4 5 6 7 0 1 2
+	//  4 5 0 1 2
+	//  3 1
 	for left < right {
+		fmt.Println(nums[left : right+1])
 		mid := left + (right-left)/2
 		if nums[mid] > nums[len(nums)-1] {
+			// keep pushing left to find
+			// the smallest number
 			left = mid + 1
-		} else {
+		}
+		if nums[mid] < nums[len(nums)-1] {
 			right = mid
+			// - If we use "right = mid - 1"
+			//	 we will miss the smallest number in some case
+			//   because [left : mid - 1] doesn't contain
+			//   smallest number when nums[mid] == smallest
+			//   eg: [4,1,2,3] -> [4].
+			// - If we assign right = nums[mid] == smallest, it
+			//   will stay fixed until the end of the loop.
+			//   So we can leave it there and pushing left
+			//   until left == right
 		}
 	}
+
 	return max(0, left-1)
 }
--- cmd/34/main.go
+++ cmd/34/main.go
@@ -12,10 +12,10 @@ func main() {
 			arr:    []int{5, 7, 7, 8, 8, 10},
 			target: 6,
 		},
-		// {
-		// 	arr:    []int{},
-		// 	target: 0,
-		// },
+		{
+			arr:    []int{1},
+			target: 0,
+		},
 	}
 
 	for _, test := range tests {
@@ -34,28 +34,66 @@ func searchRange(nums []int, target int) []int {
 		return []int{-1, -1}
 	}
 
-	start := 0
-	end := len(nums) - 1
+	low := searchLowBound(nums, target)
+	high := searchHighBound(nums, target)
 
-	// arr:    []int{5, 7, 7, 8, 8, 10},
-	for start < end {
-		if nums[start] == nums[end] && nums[start] == target {
-			break
-		}
-		mid := (start + end) / 2
-		fmt.Printf("%d %d %d\n", start, end, mid)
-		if nums[mid] < target {
-			start = mid + 1
-		} else if nums[mid] > target {
-			end = mid - 1
-		} else {
-			fmt.Printf("mid: %d", nums[mid])
-			break
+	return []int{low, high}
+}
+
+func searchLowBound(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := left + (right-left)/2
+		if target > nums[mid] {
+			left = mid + 1
+		} else if target < nums[mid] {
+			right = mid - 1
+		} else if target == nums[mid] {
+			right = mid - 1
+			// keep pushing right until nums[right + 1] == target
+			// eg: target = 2
+			// 1 2 2 2 3 3 3
+			// |
+			// right, left
+			// so eventually left will end up in front of
+			// right and the loop is finish
+			// that's why we need to check left <= right
+			// instead of left < right in normal binary search
 		}
 	}
 
-	if nums[start] == nums[end] && nums[start] == target {
-		return []int{start, end}
+	if left == len(nums) || nums[left] != target {
+		return -1
 	}
-	return []int{-1, -1}
+	return left
+}
+
+func searchHighBound(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := (left + right) / 2
+		if target > nums[mid] {
+			left = mid + 1
+		} else if target < nums[mid] {
+			right = mid - 1
+		} else if target == nums[mid] {
+			// keep pushing left until nums[left - 1] == target
+			// eg: target = 2
+			// ... 2 2 2 3 3 3
+			//           |   |
+			//          left right
+			// so eventually right will end up just right
+			// behind left and the loop is finish
+			// that's why we need to check left <= right
+			// instead of left < right in normal binary search
+			left = mid + 1
+		}
+	}
+
+	if right == -1 || nums[right] != target {
+		return -1
+	}
+	return right
 }
