9aaa83983c051e44769ba637a2dfff4246af249d
remvn <34063162+remvn@users.noreply.github.com>
2024-03-07 13:00:31 +0700

    problem 876

---
+++ cmd/876/main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	return slow
+}
