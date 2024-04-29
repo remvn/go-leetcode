e0185e1c81c83ec522ac16c3170c334186badadb
remvn <34063162+remvn@users.noreply.github.com>
2024-04-29 18:07:18 +0700

    513 create tree with bfs

--- cmd/513/breadth-first-search/main.go
+++ cmd/513/breadth-first-search/main.go
@@ -1,16 +1,59 @@
 package main
 
 import (
+	"fmt"
+
 	llq "github.com/emirpasic/gods/queues/linkedlistqueue"
 )
 
 func main() {
+	arr := []int{
+		1, 2, 3, 4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 5, 6, -1, -1, 7,
+	}
+	root := createTree(arr)
+	printTree(root)
+	fmt.Println()
 }
 
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
+}
+
+func createTree(arr []int) *TreeNode {
+	root := &TreeNode{Val: arr[0]}
+	i := 1
+	queue := []*TreeNode{root}
+	next_left := true
+	for i < len(arr) && len(queue) > 0 {
+		node := queue[0]
+		if arr[i] >= 0 {
+			if next_left {
+				node.Left = &TreeNode{Val: arr[i]}
+				queue = append(queue, node.Left)
+			} else {
+				node.Right = &TreeNode{Val: arr[i]}
+				queue = append(queue, node.Right)
+			}
+		}
+		if !next_left {
+			queue = queue[1:]
+		}
+		next_left = !next_left
+		i++
+	}
+	return root
+}
+
+func printTree(node *TreeNode) {
+	if node == nil {
+		return
+	}
+
+	printTree(node.Left)
+	fmt.Printf("%d ", node.Val)
+	printTree(node.Right)
 }
 
 func findBottomLeftValue(root *TreeNode) int {
