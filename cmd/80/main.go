9cab2ea97a73ae7c31941d97522aabc232649998
remvn <34063162+remvn@users.noreply.github.com>
2024-10-02 23:57:16 +0700

    80

---
+++ cmd/80/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{0, 0, 1, 1, 1, 1, 2, 3, 3}
+	length := removeDuplicates(arr)
+	fmt.Println(length, arr)
+}
+
+// 1 1 1 2 2 3
+// 1 1 2 2 3
+
+// i + tripLength;
+
+func removeDuplicates(nums []int) int {
+	length := len(nums)
+	count := 1
+	for i := 1; i < length; i++ {
+		if nums[i] == nums[i-1] {
+			count++
+		} else {
+			count = 1
+		}
+
+		if count == 3 {
+			count = 1
+			trimLength := 1
+			for j := i + 1; j < length; j++ {
+				if nums[j] == nums[i] {
+					trimLength++
+				} else {
+					break
+				}
+			}
+
+			for j := i; j < length; j++ {
+				if j+trimLength >= length {
+					break
+				}
+				nums[j] = nums[j+trimLength]
+			}
+
+			length -= trimLength
+		}
+	}
+
+	return length
+}
